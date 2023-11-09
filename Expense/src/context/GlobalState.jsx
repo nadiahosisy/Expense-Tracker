38fd46209cc8e5f29589b0228331276c877a5382
Nadia <nadiahosisy@gmail.com>
2023-11-09 10:50:10 +0200

    fix bug

--- Expense/src/context/GlobalState.jsx
+++ Expense/src/context/GlobalState.jsx
@@ -24,7 +24,7 @@ export const GlobalProvider = ({ children }) => {
   }
   function addTransaction(transaction) {
     dispatch({
-      type: "Add_TRANSACTION",
+      type: "ADD_TRANSACTION",
       payload: transaction,
     });
   }
